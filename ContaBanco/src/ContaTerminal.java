e40eeb2fc2074c74955dcc48cada77ec09a88d25
LuisSvntxs <luisscopo@gmail.com>
2023-06-29 13:33:11 -0300

    Finalizando Desafio ContaBanco

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Informe o seu nome: ");
+        String nomeCliente = scan.nextLine(); 
+
+        System.out.println("Informe a agência: ");
+        String agencia = scan.nextLine(); 
+
+        System.out.println("Informe o número da conta: ");
+        int numero = scan.nextInt(); 
+
+        System.out.println("Informe o seu saldo: ");
+        Double saldo = scan.nextDouble(); 
+
+        System.out.println();
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo R$"+ saldo +" já está disponível para saque");
+
+         scan.close();
     }
 }
